a7180c47a10ad7cea6b44debe51f09103768375e
Justin Beall <jus.beall@gmail.com>
2017-11-12 20:47:58 -0500

    Remove StripeRESTWebServiceDev3l reference

--- projects/StripeRESTWebServiceDev3l/src/main/java/com/dev3l/credit_payment/ws/ResourceConstants.java
+++
@@ -1,6 +0,0 @@
-package com.dev3l.credit_payment.ws;
-
-public class ResourceConstants {
-	public static final String ROOT_PATH_PAYMENT = "payment";
-	public static final String PAYMENT_PATH_CHARGE = "/charge";
-}
